a0321c0ac0898439c72fc45fc79b013161999e42
Ayush Raj <156875608+noah35802@users.noreply.github.com>
2025-02-10 19:20:51 +0530

    Create 94. Binary Tree Inorder Traversal.py
    Given the root of a binary tree, return the inorder traversal of its nodes' values.

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res = []
+        curr = root
+        while curr:
+            if curr.left is None:
+                res.append(curr.val)
+                curr = curr.right
+            else:
+                pre = curr.left
+                while pre.right and pre.right != curr:
+                    pre = pre.right
+                if pre.right is None:
+                    pre.right = curr
+                    curr = curr.left
+                else:
+                    pre.right = None
+                    res.append(curr.val)
+                    curr = curr.right
+        return res 
+
+        
